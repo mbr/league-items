47abd761e5ecead87d67ad298f609ab61f835182
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-28 15:51:27 +0200

    Added sample file.

---
+++ item_build_printer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import sys
+import yaml
+
+import networkx
+
+from lolitems import ItemGraph
+from scrape import item_file_name
+
+class Inventory(object):
+	def __init__(self, db, items = []):
+		self.db = db
+		self.items = items
+
+	def purchase(self, item):
+		cost = 0
+		for need in self.db.successors(item):
+			# we need these items before we can buy our desired item
+			if not need in self.items:
+				cost += self.purchase(need)
+
+			self.items.remove(need)
+
+		# got all the requirements, now purchase real item
+		self.buy(item)
+		self.items.append(item)
+
+		return cost + self.db.node[item]['cost']
+
+	def buy(self, name):
+		print "Buying %s for %r" % (name, self.db.node[name]['cost'])
+
+	def __repr__(self):
+		return "Inventory(%r)" % self.items
+
+
+class TrackingInventory(Inventory):
+	def __init__(self, *args, **kwargs):
+		super(TrackingInventory, self).__init__(*args, **kwargs)
+		self.recently_bought = []
+
+	def buy(self, name):
+		self.recently_bought.append(name)
+
+
+if 2 != len(sys.argv):
+	print "usage: %s itembuild.yaml" % (sys.argv[0])
+	sys.exit(1)
+
+db = ItemGraph.from_item_file(item_file_name)
+build = yaml.load(file(sys.argv[1]))
+inv = TrackingInventory(db)
+
+for item in build:
+	cost = str(inv.purchase(item))
+	if inv.recently_bought:
+		cost += ' (=%s)' % "+".join((str(db.node[n]['cost']) for n in inv.recently_bought))
+	inv.recently_bought = []
+	print "- %s # %s" % (item, cost)
