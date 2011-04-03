a2c6c48d04beed253f9d4c0396cc37d46b251530
Marc Brinkmann <git@marcbrinkmann.de>
2011-04-03 11:21:49 +0200

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from distutils.core import setup
+
+setup(name = 'leagueoflegends',
+      version = '0.1dev',
+      description = 'Utilities for creating websites related to League of Legends.',
+      author = 'Marc Brinkmann',
+      url = 'https://github.com/mbr/league-items',
+      packages = ['leagueoflegends'],
+      install_requires = ['networkx>=1.2'],
+      scripts = ['download-items', 'item-build-printer'],
+     )
